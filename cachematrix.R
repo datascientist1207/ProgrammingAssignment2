00c8262ec1d68e170840991fd8814497029b8b9d
Miles Taylor <miles.taylor@gmail.com>
2016-01-23 14:35:22 -0600

    fingers crossed

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,13 @@
 ## functions do
 
 ## Write a short comment describing this function
+## I much preferred lsat week's assignment which had clear expectations.
+## This one, IMO, is too loose.  
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	myMatrix<<-matrix(c(1,0,0,1),2,2)
+	myMatrixInverse <<- solve(myMatrix)
 
 }
 
@@ -11,5 +16,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+
+	if (!is.null(x) ) { 
+		if ( identical(myMatrix,x)) { return(myMatrixInverse )}
+		else { return(solve(x)) }
+	}
+	else {
+		return(solve(x))
+	}
+	
         ## Return a matrix that is the inverse of 'x'
 }
