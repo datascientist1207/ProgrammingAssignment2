61513f691b0aac0141661f93e952eeeefff68a8f
Miles Taylor <miles.taylor@gmail.com>
2016-03-16 21:55:23 -0500

    Round 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The makeCacheMatrix fxn creates a list vector with a matrix and stores the inverse value of the matrix
+# if it has already been calculated.  If not it contains a null value in the getInverse fxn.
 
-## Write a short comment describing this function
-## I much preferred lsat week's assignment which had clear expectations.
-## This one, IMO, is too loose.  
 
 makeCacheMatrix <- function(x = matrix()) {
 
-#	myMatrix<<-matrix(c(1,0,0,1),2,2)
-	myMatrix<<- x
-	myMatrixInverse <<- solve(myMatrix)
+makeVector <- function(x = numeric()) {
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+
+	get <- function() x
+	setInverse <- function(inverse) m <<- inverse
+	getInverse <- function() m
+	list(set = set, get = get,
+		setInverse = setInverse,
+		getInverse = getInverse)
+														  }
 
 }
 
 
-## Write a short comment describing this function
+# Tis function first looks for the value m which was stored by getInverse in makeCacheMatrix fxn and sees  whether it's 
+# null.  If not, pull from cache, if yes calculate inverse and return it. 
 
 cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+      if(!is.null(m)){
+		message("Retrieving from cache")
+	      	return(m)
+        }
 
-	if (!is.null(x) ) { 
-		if ( identical(myMatrix,x)) { return(myMatrixInverse )}
-		else { return(solve(x)) }
-	}
-	else {
-		return(solve(x))
-	}
-	
-        ## Return a matrix that is the inverse of 'x'
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setmatrix(m)
+	m
 }
